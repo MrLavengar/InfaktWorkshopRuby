770f40fb3c25b0d2a58b8091c1d8102849b455e8
Bartosz Piekarz <47979049+MrLavengar@users.noreply.github.com>
2023-04-30 22:00:18 +0200

    solved task2

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,23 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    length_validation = (6..24).include?(@password.length)
+    uppercase_letter_validation = !!(@password =~ /[A-Z]/)
+    lowercase_letter_validation = !!(@password =~ /[a-z]/)
+    digit_validation = !!(@password =~ /\d/)
+    special_character_validation = !!(@password =~ /[!@#$%&*+=:;?<>]/)
+    repeated_symbols_validation = !(@password =~ /(\w|\W)\1{2,}/)
+
+    conditions = [length_validation, 
+      uppercase_letter_validation, 
+      lowercase_letter_validation, 
+      digit_validation, 
+      special_character_validation, 
+      repeated_symbols_validation]
+    
+    conditions.all?
+  end
 end
