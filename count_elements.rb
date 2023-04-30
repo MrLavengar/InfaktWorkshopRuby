bef02c9b72e40541c5223675aa4be38e727755a0
Bartosz Piekarz <47979049+MrLavengar@users.noreply.github.com>
2023-04-30 21:49:48 +0200

    task 1 solved

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,15 @@
 class CountElements
+  def initialize(array)
+    @array = array
+  end
 
+  def perform
+    occurences = Hash.new(0)
+    @array.each do |element|
+      occurences[element] += 1
+    end
+    occurences
+  end
 end
+
+CountElements.new([0, 4, 2, 1, 0, 2, 5]).perform
